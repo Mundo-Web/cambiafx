09a944f18573a695a7e58f00f79a84795a1a0dc3
brad-basilio <brad.basilioppy@gmail.com>
2025-10-04 10:29:43 -0500

    fix: CSP completo + Service Worker corregido para Tawk.to

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,16 +1,7 @@
 var staticCacheName = "pwa-v" + new Date().getTime();
 var filesToCache = [
-    '/offline',
-    '/css/app.css',
-    '/js/app.js',
-    '/images/icons/icon-72x72.png',
-    '/images/icons/icon-96x96.png',
-    '/images/icons/icon-128x128.png',
-    '/images/icons/icon-144x144.png',
-    '/images/icons/icon-152x152.png',
-    '/images/icons/icon-192x192.png',
-    '/images/icons/icon-384x384.png',
-    '/images/icons/icon-512x512.png',
+    // Deshabilitado temporalmente para evitar errores de cache
+    // Los archivos se cargarán bajo demanda
 ];
 
 // Cache on install
@@ -19,7 +10,11 @@ self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(staticCacheName)
             .then(cache => {
-                return cache.addAll(filesToCache);
+                // Solo cachear si hay archivos en la lista
+                if (filesToCache.length > 0) {
+                    return cache.addAll(filesToCache);
+                }
+                return Promise.resolve();
             })
     )
 });
